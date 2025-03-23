792c16d252761ed78810f16b0f915d21e05b20b2
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2025-03-24 00:14:40 +0100

    Nav menu

--- components/shadcn/navigation-menu/navigation-menu.tsx
+++ components/shadcn/navigation-menu/navigation-menu.tsx
@@ -20,7 +20,42 @@ export function NavigationMenuDemo() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>Movies</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="flex flex-col gap-2 text-center p-4">
+            <h3 className="pt-2 text-center font-bold text-lg">Movies</h3>
+            <ul className="flex flex-col gap-2 text-center p-4 w-[200px]">
+              <NavigationMenuLink asChild>
+                <Link
+                  href="/movies/popular"
+                  className="text-sm font-medium leading-none"
+                >
+                  Popular
+                </Link>
+              </NavigationMenuLink>
+              <Separator />
+              <NavigationMenuLink asChild>
+                <Link
+                  href="/movies/top-rated"
+                  className="text-sm font-medium leading-none"
+                >
+                  Top Rated
+                </Link>
+              </NavigationMenuLink>
+              <Separator />
+              <NavigationMenuLink asChild>
+                <Link
+                  href="/movies/upcoming"
+                  className="text-sm font-medium leading-none"
+                >
+                  Upcoming
+                </Link>
+              </NavigationMenuLink>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>TV Shows</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <h3 className="pt-2 text-center font-bold text-lg">TV Shows</h3>
+            <ul className="flex flex-col gap-2 text-center p-4 w-[200px]">
               <NavigationMenuLink asChild>
                 <Link
                   href="/movies/popular"
